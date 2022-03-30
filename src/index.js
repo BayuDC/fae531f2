0b313bdbaa77824519c95150d4e58224648c6d84
BayuDC <dev.bayudc@gmail.com>
2022-03-30 15:43:38 +0700

    Update the style structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
-import './styles/main.less';
+import './index.less';
 
 ReactDOM.render(
     <React.StrictMode>
